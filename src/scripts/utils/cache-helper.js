f590c313ec5dd90dc005f0fb891956317fd6a045
Hamjaya Saputra Salahuddin <hamjayasaputra35@gmail.com>
2022-11-02 21:28:05 +0800

    modified cache helper

--- src/scripts/utils/cache-helper.js
+++ src/scripts/utils/cache-helper.js
@@ -1,3 +1,5 @@
+import CONFIG from '../globals/config';
+
 const CacheHelper = {
   async cachingAppShell(requests) {
     const cache = await this.__openCache();
@@ -5,15 +7,40 @@ const CacheHelper = {
   },
 
   async deleteOldCache() {
-
+    const cacheNames = await caches.keys();
+    cacheNames
+      .filter((name) => name !== CONFIG.CACHE_NAME)
+      .map((filteredName) => caches.delete(filteredName));
   },
 
   async revalidateCache(request) {
+    const response = await caches.match(request);
 
+    if (response) {
+      this.__fetchRequest(request);
+      return response;
+    }
+    return this.__fetchRequest(request);
   },
 
   async __openCache() {
-    return caches.open('MovieCatalogue-V1');
+    return caches.open(CONFIG.CACHE_NAME);
+  },
+
+  async __fetchRequest(request) {
+    const response = await fetch(request);
+
+    if (!response || response.status !== 200) {
+      return response;
+    }
+
+    await this.__addCache(request);
+    return response;
+  },
+
+  async __addCache(request) {
+    const cache = await this.__openCache();
+    cache.add(request);
   },
 
 };
